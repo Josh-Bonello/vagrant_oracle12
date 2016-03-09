9ee3eb90214f6c9544ae66793704e24e094a648d
kmehkeri <kmehkeri@gmail.com>
2016-03-09 23:23:07 +0100

    Add IUS repository with Python 3.5

--- provision_system.sh
+++ provision_system.sh
@@ -16,11 +16,11 @@ echo ">>>> Disable firewall"
 systemctl stop firewalld
 systemctl disable firewalld
 
-echo ">>>> Install EPEL repository"
-curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh epel-release-latest-7.noarch.rpm
+echo ">>>> Install additional repositories"
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y https://rhel7.iuscommunity.org/ius-release.rpm
 
 echo ">>>> Update/install software"
 yum update -y
-yum install -y unzip gcc kernel-uek-devel wget vim git rlwrap python34
+yum install -y unzip gcc kernel-uek-devel wget vim git rlwrap python35u
 
